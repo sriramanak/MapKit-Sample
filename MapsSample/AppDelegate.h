a07a9f045c3a2081626dff5719d2273f0a78514d
paradigm creatives <paradigm@paradigms-Mac-mini.local>
2014-10-01 10:16:18 +0530

    Initial Commit

---
+++ MapsSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapsSample
+//
+//  Created by paradigm creatives on 10/1/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapsSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapsSample
+//
+//  Created by paradigm creatives on 10/1/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
